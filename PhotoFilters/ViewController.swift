0fcf34a82cc9701a4ee8a95f725f1b28d3aee58e
Kiyoshi Mizumoto <mizumoto@andgenie.jp>
2014-10-09 12:19:57 +0900

    first commit

---
+++ PhotoFilters/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  PhotoFilters
+//
+//  Created by Kiyoshi Mizumoto on 2014/10/09.
+//  Copyright (c) 2014年 Andgenie Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
+    
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        // Create an image to filter
+        let inputImage = CIImage(image: photoImageView.image)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Rendered the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        // Refletct the change back in the interface
+        photoImageView.image = UIImage(CGImage: renderedImage)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+PhotoFilters
+=
+- Xcode 6.0.1
+- Swift
+
+アップルのSwiftブログの「[Building Your First Swift App Video](https://developer.apple.com/swift/blog/?id=16)」で公開されたSwiftチュートリアルビデオに沿って実装したものです。
+by [@mizumotok](https://twitter.com/mizumotok)
+
